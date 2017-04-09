47f969a5c3504958ed2886be95d6bbf999d2b56a
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-04-09 12:56:24 +0200

    fixed obsolete files

--- src/SmartPHP/Interfaces/DataSourceInvokatorInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace SmartPHP\Interfaces;
-
-interface DataSourceInvokatorInterface
-{
-    public function invokeDataSource(DataSourceInterface $service, DataSourceMessageInterface $message): DataSourceMessageInterface;
-}
