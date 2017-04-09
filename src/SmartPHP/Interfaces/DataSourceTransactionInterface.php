4344956bad5a48f59e4a33d0d0148d892155c42a
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-04-09 17:07:23 +0200

    Fixed transaction interface siganture

--- src/SmartPHP/Interfaces/DataSourceTransactionInterface.php
+++ src/SmartPHP/Interfaces/DataSourceTransactionInterface.php
@@ -1,15 +1,14 @@
 <?php
 namespace SmartPHP\Interfaces;
 
-use SmartPHP\Models\DataSourceOperation;
-
 interface DataSourceTransactionInterface
 {
+
     public function getTransactionNum(): int;
-    
+
     public function setTransactionNum(int $transactionNum): DataSourceTransactionInterface;
-        
+
     public function getOperations(): array;
-    
-    public function addOperation(DataSourceOperation $operation): DataSourceTransactionInterface;
+
+    public function addOperation(DataSourceOperationInterface $operation): DataSourceTransactionInterface;
 }
